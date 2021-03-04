efb7d3e4b4f3e3d5fc6256ede9fab99add598328
Diego Meruoca <diegomeruoca@gmail.com>
2021-03-03 23:40:19 -0300

    Criação do mundo

--- core/src/com/diegomeruoca/plaraforma/screens/PlayScreen.java
+++ core/src/com/diegomeruoca/plaraforma/screens/PlayScreen.java
@@ -4,7 +4,9 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.diegomeruoca.plaraforma.Plataforma;
@@ -13,19 +15,26 @@ import com.diegomeruoca.plaraforma.scenes.Hud;
 public class PlayScreen implements Screen {
     private Plataforma game; // Instacia um objeto do tipo Paltaforma
     private Hud hud;
-    private OrthographicCamera camera;
-    private Viewport viewport;
+    private OrthographicCamera gamecam;
+    private Viewport gameport;
+
+    private TmxMapLoader maploader; // Para carregar nosso mapa
+    private TiledMap map; // Para armazenar nosso mapa
+    private OrthogonalTiledMapRenderer renderer; // Pra renderizar o mapa pra tela
 
     public PlayScreen(Plataforma game) {
         this.game = game;
-        camera = new OrthographicCamera(); // Cria um objeto do tipo camera
-        viewport = new FitViewport(Plataforma.V_WIDITH, Plataforma.V_HEIGHT, camera); // Cria um ajanela de exibição
+        gamecam = new OrthographicCamera(); // Cria um objeto do tipo camera
+        gameport = new FitViewport(Plataforma.V_WIDITH, Plataforma.V_HEIGHT, gamecam); // Cria um ajanela de exibição
         /*StrechViewport -  vai esticar o conteudo pra ajustar ao tamanho da janela, distorcendo o conteúdo dependendo
         da proporção.
         FitViewport -  vai aumentar o tamanho do conteudo acompanhando a janela, mas caso o tamanho não seja
         proporcional, adiciona barras pretas nas lateria ou em cima e embaixo.**/
         hud = new Hud(game.batch);
-
+        maploader = new TmxMapLoader();
+        map = maploader.load("mapa.tmx");
+        renderer = new OrthogonalTiledMapRenderer(map);
+        gamecam.position.set(gameport.getWorldWidth() / 2, gameport.getWorldHeight() / 2, 0);
     }
 
     @Override
@@ -33,17 +42,33 @@ public class PlayScreen implements Screen {
 
     }
 
+    private void handleInput(float dt) {
+        if (Gdx.input.isTouched())
+            gamecam.position.x += 100 * dt;
+    }
+
+    public void update(float dt){
+        handleInput(dt);
+        gamecam.update();
+        renderer.setView(gamecam);
+    }
+
+
+
     @Override
     public void render(float delta) {
+        update(delta);
+
         Gdx.gl.glClearColor(0,0,0,1); //Define a cor do fundo de preto
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);//PInta o fundo
+        renderer.render();
         game.batch.setProjectionMatrix(hud.stage.getCamera().combined); //Seta a matriz de de projeção, pro batch reconhecer o que e a camera "vê"
         hud.stage.draw();
     }
 
     @Override
     public void resize(int width, int height) {
-        viewport.update(width, height); //Atualiza a visualização qnd redimensionar a janela
+        gameport.update(width, height); //Atualiza a visualização qnd redimensionar a janela
     }
 
     @Override
